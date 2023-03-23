842eaae69a9bee43cc1fce37fb34a9e6641c5808
Denard Laza <lazadenard@gmail.com>
2023-03-23 15:25:47 +0200

    Added the code for the SplashActivity and created the LoginActivity class and xml file

--- app/src/main/java/com/google/mytravelapp/activities/SplashActivity.java
+++ app/src/main/java/com/google/mytravelapp/activities/SplashActivity.java
@@ -2,7 +2,9 @@ package com.google.mytravelapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.google.mytravelapp.R;
 
@@ -12,5 +14,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
     }
 }
