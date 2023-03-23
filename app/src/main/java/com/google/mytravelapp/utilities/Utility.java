e1d6ca9f26fd3833148ca495496c2c919a010fd7
Denard Laza <lazadenard@gmail.com>
2023-03-23 15:00:33 +0200

    utility class added

--- app/src/main/java/com/google/mytravelapp/utilities/Utility.java
+++ app/src/main/java/com/google/mytravelapp/utilities/Utility.java
@@ -1,2 +1,29 @@
-package com.google.mytravelapp.utilities;public class Utility {
+package com.google.mytravelapp.utilities;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utility {
+    public static boolean isValidEmail(CharSequence target) {
+        return target != null && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public static boolean isValidPassword(final String password) {
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.[0-9])(?=.[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public static boolean isValidUsername(String name) {
+        String regex = "^[A-Za-z]\\w{5,29}$";
+        Pattern p = Pattern.compile(regex);
+        if (name == null) {
+            return false;
+        }
+        Matcher m = p.matcher(name);
+        return m.matches();
+    }
 }
