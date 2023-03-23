c09fe0542d00032d16460a09892f22683fc7c81e
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-03-23 15:03:21 +0200

    updated file structure and moved MainActivity to the activities package

--- app/src/main/java/com/google/mytravelapp/activities/MainActivity.java
+++ app/src/main/java/com/google/mytravelapp/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.google.mytravelapp;
+package com.google.mytravelapp.activities;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +14,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.mytravelapp.R;
 import com.google.mytravelapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
