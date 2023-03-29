9c469a046e0f9270b8210024df1fe344acc99b59
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-03-29 21:52:49 +0300

    created the design for the register screen and also some logic

--- app/src/main/java/com/google/mytravelapp/activities/RegisterActivity.java
+++ app/src/main/java/com/google/mytravelapp/activities/RegisterActivity.java
@@ -3,14 +3,35 @@ package com.google.mytravelapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.mytravelapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText emailAddressReg, usernameReg, passwordReg, repassReg;
+    Button registerButtonReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    private boolean validateInputs(String email, String username, String password, String repass) {
+        if (!email.isEmpty() && !username.isEmpty() && !password.isEmpty() && !repass.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private void setupViews() {
+        emailAddressReg = findViewById(R.id.emailAddressReg);
+        usernameReg = findViewById(R.id.usernameReg);
+        passwordReg = findViewById(R.id.passwordReg);
+        registerButtonReg = findViewById(R.id.registerButtonReg);
+        repassReg = findViewById(R.id.repassReg);
+    }
+
 }
