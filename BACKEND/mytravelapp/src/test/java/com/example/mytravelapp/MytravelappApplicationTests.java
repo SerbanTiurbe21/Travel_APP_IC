803e5885cecfa053ae5c5edf0c155b8b1de7dc62
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-04-06 21:14:01 +0300

    added the folder called Backend that contains the initial structure of the Spring project

---
+++ BACKEND/mytravelapp/src/test/java/com/example/mytravelapp/MytravelappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytravelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytravelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
