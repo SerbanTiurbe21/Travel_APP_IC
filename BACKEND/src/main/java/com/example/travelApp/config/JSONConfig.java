a6b9327dd07abd3320baffb225907d25eaceb035
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-04-25 23:19:40 +0300

    added the class JSONconfig

---
+++ BACKEND/src/main/java/com/example/travelApp/config/JSONConfig.java
@@ -0,0 +1,16 @@
+package com.example.travelApp.config;
+
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class JSONConfig {
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.featuresToEnable(SerializationFeature.INDENT_OUTPUT);
+        return builder;
+    }
+}
