4285eda141e2f447b58d4170faf03f94fc05c7cf
Denard Laza <lazadenard@gmail.com>
2023-04-27 17:28:05 +0300

    added the frontend for add_destination activity

---
+++ zFRONTEND/app/src/main/java/com/google/mytravelapp/activities/AddDestinationActivity.java
@@ -0,0 +1,16 @@
+package com.google.mytravelapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.mytravelapp.R;
+
+public class AddDestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_destination);
+    }
+}
