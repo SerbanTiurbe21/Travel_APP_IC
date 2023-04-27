160adfb0c499d2d7a5dd845232000139a45dd6d9
Denard Laza <lazadenard@gmail.com>
2023-04-27 17:12:47 +0300

    updated the interface for UserService

--- zFRONTEND/app/src/main/java/com/google/mytravelapp/api/user/UserService.java
+++ zFRONTEND/app/src/main/java/com/google/mytravelapp/api/user/UserService.java
@@ -1,12 +1,32 @@
 package com.google.mytravelapp.api.user;
 
 import com.google.mytravelapp.database.User;
+import com.google.mytravelapp.entities.UserDB;
+import com.google.mytravelapp.login.LoginRequest;
+import com.google.mytravelapp.login.LoginResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Query;
 
 public interface UserService {
-    @POST("api/users")
-    Call<User> registerUser(@Body User user);
+    @POST("api/users/register")
+    Call<UserDB> registerUser(@Body UserDB user);
+
+    @POST("api/users/login")
+    Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
+
+    @PUT("api/users/reset-password")
+    Call<Void> resetPassword(
+            @Query("email") String email,
+            @Query("oldPassword") String oldPassword,
+            @Query("newPassword") String newPassword,
+            @Query("confirmNewPassword") String confirmNewPassword
+    );
+
+    @GET("api/users/find-by-email")
+    Call<User> getUserByEmail(@Query("email") String email);
 }
