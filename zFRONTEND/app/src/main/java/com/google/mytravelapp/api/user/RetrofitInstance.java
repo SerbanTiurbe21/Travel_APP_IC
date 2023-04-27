07f002e2e80494516a2c9c36a374aca25df34a69
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-04-27 17:46:38 +0300

    updated some things

--- zFRONTEND/app/src/main/java/com/google/mytravelapp/api/user/RetrofitInstance.java
+++ zFRONTEND/app/src/main/java/com/google/mytravelapp/api/user/RetrofitInstance.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://localhost:8080/api/users";
+    private static final String BASE_URL = "http://localhost:8080/api/";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
