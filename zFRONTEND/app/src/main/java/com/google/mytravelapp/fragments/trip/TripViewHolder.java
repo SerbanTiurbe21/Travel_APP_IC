b89c647c13a77397f8adc3f10ce9659180083983
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-05-11 15:43:34 +0300

    added modifications

--- zFRONTEND/app/src/main/java/com/google/mytravelapp/fragments/trip/TripViewHolder.java
+++ zFRONTEND/app/src/main/java/com/google/mytravelapp/fragments/trip/TripViewHolder.java
@@ -1,4 +1,54 @@
 package com.google.mytravelapp.fragments.trip;
 
-public class TripViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.mytravelapp.R;
+
+public class TripViewHolder extends RecyclerView.ViewHolder {
+    private final ImageView imageView2, bookmarkIconCustomRow;
+    private final TextView tripNameCustomRow, destinationCustomRow, textView9, ratingCustomRow, emailCustomRow;
+
+    public TripViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.imageView2 = itemView.findViewById(R.id.imageView2);
+        this.bookmarkIconCustomRow = itemView.findViewById(R.id.bookmarkIconCustomRow);
+        this.tripNameCustomRow = itemView.findViewById(R.id.tripNameCustomRow);
+        this.destinationCustomRow = itemView.findViewById(R.id.destinationCustomRow);
+        this.textView9 = itemView.findViewById(R.id.textView9);
+        this.ratingCustomRow = itemView.findViewById(R.id.ratingCustomRow);
+        this.emailCustomRow = itemView.findViewById(R.id.emailCustomRow);
+    }
+
+    public ImageView getImageView2() {
+        return imageView2;
+    }
+
+    public ImageView getBookmarkIconCustomRow() {
+        return bookmarkIconCustomRow;
+    }
+
+    public TextView getTripNameCustomRow() {
+        return tripNameCustomRow;
+    }
+
+    public TextView getDestinationCustomRow() {
+        return destinationCustomRow;
+    }
+
+    public TextView getTextView9() {
+        return textView9;
+    }
+
+    public TextView getRatingCustomRow() {
+        return ratingCustomRow;
+    }
+
+    public TextView getEmailCustomRow() {
+        return emailCustomRow;
+    }
 }
--- zFRONTEND/app/src/main/java/com/google/mytravelapp/fragments/trip/TripViewModel.java
+++ zFRONTEND/app/src/main/java/com/google/mytravelapp/fragments/trip/TripViewModel.java
@@ -1,4 +1,19 @@
 package com.google.mytravelapp.fragments.trip;
 
-public class TripViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class TripViewModel extends ViewModel {
+    private MutableLiveData<List<Trip>> dataSource = new MutableLiveData<>();
+
+    public void setDataSource(List<Trip> dataSource) {
+        this.dataSource.setValue(dataSource);
+    }
+
+    public LiveData<List<Trip>> getDataSource() {
+        return dataSource;
+    }
 }
